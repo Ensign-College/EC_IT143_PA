ec4154e4f4df83467105ace2a9f27efbbe9e545d
sirprince1951985-ai <sirprince1951985@gmail.com>
2026-06-01 14:19:05 +0000

    Add files via upload

---
+++ EC_IT143_W5.2_Simpsons_pa.sql
@@ -0,0 +1,28 @@
+/* =========================================
+   EC_IT143 Week 5.2 Assignment
+   Dataset: Simpsons
+   Student: Prince Agyei
+   ========================================= */
+
+   -- Q1: How many players are in the team?
+
+SELECT COUNT(*) AS TotalPlayers
+FROM dbo.tblplayerdim;
+
+
+-- Q2: How many teams are there?
+
+SELECT COUNT(*) AS TotalTeams
+FROM dbo.tblteamdim;
+
+
+-- Q3: Which player has the highest salary?
+
+SELECT TOP 1 Name, Salary
+FROM Players
+ORDER BY Salary DESC;
+
+-- Q2: What is the average age of players?
+
+SELECT AVG(Age) AS AvgAge
+FROM Players;
